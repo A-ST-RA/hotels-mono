7c70739e486f62f0418962f074db3ef25d43d6aa
A_ST_RA <astrax200469@gmail.com>
2024-04-14 18:09:55 +0300

    feat(hotels-api): [WIP] Hotels Api creating
    Added base hotels api
    Added plugin untransformeer

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface HotelComponentsRoom extends Schema.Component {
+  collectionName: 'components_hotel_components_rooms';
+  info: {
+    displayName: 'Room';
+    icon: 'book';
+  };
+  attributes: {
+    title: Attribute.String;
+    slug: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'hotel-components.room': HotelComponentsRoom;
+    }
+  }
 }
